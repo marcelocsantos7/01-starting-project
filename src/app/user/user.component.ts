55b0ac075c449a883093a48f2985405004349a7c
Marcelo Cesar dos Santos <marcelocsantos7@gmail.com>
2024-09-02 09:46:40 -0300

    add inputs and outputs properties

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,8 +8,24 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  public selectedUser = DUMMY_USERS[randomIndex];
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // imagePath = computed(() => 'assets/users/' + this.avatar);
+  
+  // public selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter<string>(); 
+  // select = output<string>();
+
   get imagePath(){
-    return 'assets/users/' + this.selectedUser.avatar;
+      return 'assets/users/' + this.avatar;
+    }
+  onSelectUser(){
+    this.select.emit(this.id);
   }
+
 }
